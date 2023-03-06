dd859bd1f69257b6e4bd03b8c735e51378eaf1d7
danisec <daniaprilyanto35@gmail.com>
2023-03-06 08:56:35 +0700

    update: web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', fn() => view ('pages.home.index'));
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
+
+Route::post('/logout', [LoginController::class, 'logout'])->name('login.logout');
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
