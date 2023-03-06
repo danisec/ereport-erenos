73dd01752f960039e076a4dbc7ccabc102b847d8
danisec <daniaprilyanto35@gmail.com>
2023-03-06 08:46:43 +0700

    add: component breadcrumb

---
+++ resources/views/components/molecules/breadcrumb.blade.php
@@ -0,0 +1,23 @@
+@props(['title'])
+
+<nav aria-label="Breadcrumb" class="layout flex h-32 items-center justify-start">
+    <ol class="inline-flex items-center space-x-1 md:space-x-3">
+        <li class="inline-flex items-center">
+            <a class="inline-flex items-center text-xl font-light text-white/70 hover:font-medium hover:text-white"
+                href="/">
+                Home
+            </a>
+        </li>
+        <li aria-current="login">
+            <div class="flex items-center">
+                <svg aria-hidden="true" class="h-6 w-6 text-white/70" fill="currentColor" viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path clip-rule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        fill-rule="evenodd"></path>
+                </svg>
+                <span class="ml-1 text-xl font-light text-white/70 md:ml-2">{{ $title }}</span>
+            </div>
+        </li>
+    </ol>
+</nav>
