8a032a21bd208d1d470d3930cff88b452a38ef13
danisec <daniaprilyanto35@gmail.com>
2023-03-06 08:46:54 +0700

    add: component header

--- resources/views/components/organisms/header.blade.php
+++ resources/views/components/organisms/header.blade.php
@@ -1,4 +1,24 @@
-<div class="flex gap-6 justify-center text-white py-8 px-4 bg-blue-800">
-    <a href="/">Home</a>
-    <a href="/posts">Posts</a>
-</div>
+<header class="flex h-28 w-full items-center justify-between bg-header pr-11">
+    <div class="flex flex-row items-center justify-start gap-4">
+        <div>
+            <x-atoms.logo classLogo="h-auto w-28 object-cover" />
+        </div>
+
+        <div class="flex flex-col items-start gap-2">
+            <p class="text-3xl font-semibold uppercase leading-9 text-white">Sekolah Erenos</p>
+            <p class="text-3xl font-semibold leading-9 text-white">Iman, Ilmu dan Damai Sejahtera</p>
+        </div>
+    </div>
+
+    @auth
+        <form action="{{ route('login.logout') }}" method="post">
+            @csrf
+            <button class="btn-header">Logout</button>
+        </form>
+    @else
+        <a href="/login">
+            <button class="btn-header">Login</button>
+        </a>
+    @endauth
+
+</header>
