03d2e5f519c5dc25e73c82dd092910aea89cf85f
danisec <daniaprilyanto35@gmail.com>
2023-03-30 08:33:44 +0700

    update: web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ChangePasswordController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\SiswaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,12 +19,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::controller(HomeController::class)->name('home.')->group(function () {
+    Route::get('/', 'index')->name('index');
+});
 
-Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
-Route::post('/login', [LoginController::class, 'authenticate'])->name('login');
+Route::controller(LoginController::class)->group(function () {
+    Route::get('/login', 'index')->name('login')->middleware('guest');
+    Route::post('/login', 'authenticate')->name('login.authenticate')->middleware('guest');
 
-Route::post('/logout', [LoginController::class, 'logout'])->name('login.logout');
+    Route::post('/logout', 'logout')->name('login.logout')->middleware('auth');
+});
 
-Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
-Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+Route::controller(RegisterController::class)->name('register.')->middleware('guest')->group(function () {
+    Route::get('/register', 'index')->name('index');
+    Route::post('/register', 'store')->name('store');
+});
+
+Route::controller(ChangePasswordController::class)->name('changePassword.')->middleware('auth')->group(function () {
+    Route::get('/change-password', 'index')->name('index');
+    Route::post('/change-password', 'update')->name('update');
+});
+
+Route::controller(DashboardController::class)->name('dashboard.')->middleware('auth')->group(function () {
+    Route::get('/dashboard', 'index')->name('index');
+});
+
+Route::controller(SiswaController::class)->name('siswa.')->middleware('auth')->group(function () {
+    Route::get('/dashboard/siswa', 'index')->name('index');
+    Route::get('/dashboard/tambah-siswa', 'create')->name('create');
+
+    Route::post('/dashboard/tambah-siswa', 'store')->name('store');
+    Route::delete('/dashboard/siswa/{id}', 'destroy')->name('destroy');
+
+    Route::get('/dashboard/ubah-siswa/{id}/edit', 'edit')->name('edit');
+    Route::put('/dashboard/siswa/{id}', 'update')->name('update');
+});
