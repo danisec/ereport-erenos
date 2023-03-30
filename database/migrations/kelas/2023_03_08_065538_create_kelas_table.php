7b41725d043bdfadb113f307052ed553b3dee8fe
danisec <daniaprilyanto35@gmail.com>
2023-03-30 08:51:49 +0700

    migration: create table kelas

---
+++ database/migrations/kelas/2023_03_08_065538_create_kelas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kelas', function (Blueprint $table) {
+            $table->id('id_kelas');
+            $table->char('nisn', 8);
+            $table->char('tahun_ajaran', 5);
+            $table->char('nis', 8);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kelas');
+    }
+};
