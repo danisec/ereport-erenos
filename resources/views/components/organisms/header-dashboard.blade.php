b9cfd28e1b87c74c827276685f03733d7b22fc5a
danisec <daniaprilyanto35@gmail.com>
2023-03-30 08:41:35 +0700

    add: header-dashboard.blade.php

---
+++ resources/views/components/organisms/header-dashboard.blade.php
@@ -0,0 +1,12 @@
+    <nav class="h-28 w-full bg-login-pattern bg-cover bg-no-repeat">
+        <ul class="layout flex h-28 items-center justify-start">
+            <li class="flex flex-row items-center gap-12">
+                @foreach ($navbar as $name => $url)
+                    <a class="{{ request()->segment(2) == $url
+                        ? 'text-white font-semibold text-xl leading-7 bg-nav-active/40 rounded-full py-2 px-5 transition-all ease-in'
+                        : 'text-white font-semibold text-xl leading-7' }}"
+                        href="{{ $url == '/' ? url('') : url('') . '/dashboard' . '/' . $url }}">{{ $name }}</a>
+                @endforeach
+            </li>
+        </ul>
+    </nav>
