314c1800b81159af72a9ece7c274ae56e7df3561
danisec <daniaprilyanto35@gmail.com>
2023-03-30 08:50:57 +0700

    migration: add foreign key jadwal

---
+++ database/migrations/jadwal/2023_03_08_073405_add_fk_jadwal.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('jadwals', function (Blueprint $table) {
+            $table->foreignId('id_level', 'fk_jadwal_level')
+                ->references('id_level')
+                ->on('level')
+                ->restrictOnUpdate()
+                ->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jadwal', function (Blueprint $table) {
+            $table->dropForeign('fk_jadwal_level');
+        });
+    }
+};
