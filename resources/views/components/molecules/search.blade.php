58622c71cc39f6848d8310705a8aa8fed58ef4e9
danisec <daniaprilyanto35@gmail.com>
2023-03-30 08:42:02 +0700

    add: search.blade.php

---
+++ resources/views/components/molecules/search.blade.php
@@ -0,0 +1,20 @@
+<form class="my-4" action="" method="GET">
+    @if (request('nama_siswa'))
+        <input name="nama_siswa" type="hidden" value="{{ request('nama_siswa') }}">
+    @endif
+
+    <label class="sr-only mb-2 text-sm font-medium text-gray-900" for="default-search">Search</label>
+
+    <div class="relative">
+        <input
+            class="block w-full rounded-md border border-gray-300 bg-white py-2.5 px-10 pl-3 text-sm font-medium text-gray-900 focus:outline-none"
+            name="search" value="{{ request('search') ?? '' }}" placeholder="Cari Siswa">
+
+        <button class="absolute right-2.5 bottom-2.5 focus:outline-none" type="submit">
+            <svg class="h-6 w-6 text-gray-500" aria-hidden="true" fill="none" stroke="currentColor"
+                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="2"></path>
+            </svg></button>
+    </div>
+</form>
