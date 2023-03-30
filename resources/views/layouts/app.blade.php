667fbc0382d995e59fc7698deb79448faa3d142b
danisec <daniaprilyanto35@gmail.com>
2023-03-30 08:39:41 +0700

    add: link font rubik

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,8 @@
 
 <head>
     <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link href="{{ asset('favico.svg') }}" rel="shortcut icon" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="image/x-icon" href="{{ asset('favico.svg') }}" rel="shortcut icon">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
     <link
         href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&family=Roboto:wght@300;400;500;700;900&display=swap"
@@ -14,7 +14,11 @@
         rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
+
     <title>{{ $title }} - Sekolah Erenos</title>
+
     @notifyCss
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
