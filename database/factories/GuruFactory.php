104ee9daacee10ecd400f75e16b42103dbb2bf27
danisec <daniaprilyanto35@gmail.com>
2023-04-06 22:47:42 +0700

    add: GuruFactory.php

---
+++ database/factories/GuruFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Guru>
+ */
+class GuruFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nig' => fake()->randomNumber(8),
+            'nama_guru' => fake()->name(),
+        ];
+    }
+}
