7b6aeee07604c7208b90d94450f5508ca3792ae4
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:44:58 +0700

    update: avatar.blade.php

--- resources/views/components/molecules/avatar.blade.php
+++ resources/views/components/molecules/avatar.blade.php
@@ -4,7 +4,7 @@
     </button>
 
     <div class="absolute right-0 mr-14 mt-2 w-64 rounded-lg bg-white shadow-md shadow-slate-100" x-show="show"
-        x-on:click.outside="show = !show" x-bind:class="hidden: !show"
+        x-on:click.outside="show = !show" x-bind:class="hidden = !show"
         x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95"
         x-transition:enter-end="transform opacity-100 scale-100" x-cloak>
         <div class="flex flex-col gap-3 p-4">
