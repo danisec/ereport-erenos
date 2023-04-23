8998efa81465698bdeb318a26a97a385e622c675
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:33:49 +0700

    update: Siswa.php

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -10,32 +10,29 @@ class Siswa extends Model
 {
     use HasFactory, Sortable;
 
+    protected $table = 'siswa';
+
     protected $fillable = [
-        'nis',
-        'nama_siswa',
-        'nama_panggilan',
-        'tinggi_badan',
-        'berat_badan',
+        'NIS',
+        'nmSiswa',
+        'nmPanggil',
+        'tinggi',
+        'berat',
     ];
 
     public $sortable = [
-        'nis',
-        'nama_siswa',
-        'nama_panggilan',
-        'tinggi_badan',
-        'berat_badan',  
-        'created_at', 
-        'updated_at'
+        'NIS',
+        'nmSiswa',
+        'nmPanggil',
+        'tinggi',
+        'berat',  
     ];
 
     public function scopeFilter($query, array $filters)
     {  
         $query->when($filters['search'] ?? false, fn($query, $search) =>
-            $query->where('nama_siswa', 'like', '%'. $search . '%')
-        );
-
-        $query->when($filters['nama_panggilan'] ?? false, fn($query, $nama_panggilan) =>
-            $query->where('nama_panggilan', 'like', '%'. $nama_panggilan . '%')
+            $query->where('berat', 'like', '%'. $search . '%')
+                ->orWhere('nmPanggil', 'like', '%' . $search . '%')
         );
     }
 }
