5b3138372e444fefed658ae2a9d82e16462815ec
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:33:20 +0700

    add: Model Pelajaran.php

---
+++ app/Models/Pelajaran.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
+
+class Pelajaran extends Model
+{
+    use HasFactory, Sortable;
+
+    protected $table = 'pelajaran';
+
+    protected $fillable = [
+        'kodePelajaran',
+        'nmPelajaran',
+        'nmSingkatan',
+        'KKM',
+    ];
+
+    public $sortable = [
+        'kodePelajaran',
+        'nmPelajaran',
+        'nmSingkatan',
+        'KKM',
+    ];
+
+    public function scopeFilter($query, array $filters)
+    {  
+        $query->when($filters['search'] ?? false, fn ($query, $search) =>
+            $query->where('nmPelajaran', 'like', '%' . $search . '%')
+                        ->orWhere('nmSingkatan', 'like', '%' . $search . '%')
+        );
+    }
+}
