8cdb1d834793b7846808696195c44540465499c4
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:49:28 +0700

    add: edit.blade.php

---
+++ resources/views/pages/dashboard/kelas/edit.blade.php
@@ -0,0 +1,44 @@
+<x-layouts.app-layout title="{{ $title }}">
+
+    <x-organisms.header-dashboard />
+
+    <section class="layout min-h-screen bg-hero py-14 font-rubik">
+        <div class="w-full rounded-2xl bg-white py-5 shadow-sm">
+
+            <div class="w-6/12 rounded-r-2xl bg-hero py-2">
+                <h4 class="text-gray-9000 px-11 text-2xl font-bold text-white">Mengubah Data Kelas</h4>
+            </div>
+
+            <form class="flex flex-col gap-6 px-11 pt-9" action="/dashboard/kelas/{{ $kelas->idKelas }}" method="post">
+                @method('put')
+                @csrf
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-center">
+                        <label class="mb-2 w-64 text-xl font-medium leading-9">Nama Kelas</label>
+                        <input class="@error('kelas') border-red-300 bg-red-300 @enderror field-input-blue w-8/12"
+                            name="kelas" type="text" value="{{ $kelas->kelas }}" placeholder="Nama" required>
+                    </div>
+
+                    @error('kelas')
+                        <p class="invalid-feedback ml-64">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="mt-32 flex flex-row items-center gap-14">
+                    <button
+                        class="ml-64 rounded-sm bg-cyan-300 px-10 py-3 text-center text-sm font-normal shadow-xl shadow-gray-300">Update</button>
+
+                    <a class="rounded-sm bg-cyan-300 px-10 py-3 text-center text-sm font-normal shadow-xl shadow-gray-300"
+                        href="{{ URL('dashboard/kelas') }}">Cancel</a>
+
+                </div>
+            </form>
+
+        </div>
+
+    </section>
+
+</x-layouts.app-layout>
