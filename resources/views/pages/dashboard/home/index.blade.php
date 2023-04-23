609356665c83c08a4e788c6c909f9011b646d4dc
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:49:02 +0700

    add: index.blade.php

---
+++ resources/views/pages/dashboard/home/index.blade.php
@@ -0,0 +1,9 @@
+<x-layouts.app-layout title="{{ $title }}">
+
+    <x-organisms.header-dashboard />
+
+    <section class="layout mt-14">
+        <h2>Dashboard</h2>
+    </section>
+
+</x-layouts.app-layout>
