9f1955efee596e3b7f3092e3d760a7c7b3adcccc
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:42:40 +0700

    add: KelasSeeder.php

---
+++ database/seeders/KelasSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Kelas;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class KelasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Kelas::factory()->count(12)->create();
+    }
+}
