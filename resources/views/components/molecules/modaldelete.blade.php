f1057a9c43e766f1d1212abbad36f1d84fe830c4
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:45:11 +0700

    update: modaldelete.blade.php

--- resources/views/components/molecules/modaldelete.blade.php
+++ resources/views/components/molecules/modaldelete.blade.php
@@ -1,12 +1,12 @@
 @props(['title', 'action'])
 
 <div class="fixed left-0 right-0 top-2/4 z-20 mx-auto w-3/4 border-4 border-blue-700 bg-hero py-6" x-show="show"
-    x-on:click.outside="show = !show" x-bind:class="hidden: !show" x-transition:enter="transition ease-out duration-100"
+    x-on:click.outside="show = !show" x-bind:class="hidden = !show" x-transition:enter="transition ease-out duration-100"
     x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100"
     x-cloak>
 
     <div class="flex flex-col gap-4 text-center">
-        <h4 class="text-2xl font-normal text-white">HAPUS
+        <h4 class="text-2xl font-normal uppercase text-white">HAPUS
             DATA
         </h4>
 
