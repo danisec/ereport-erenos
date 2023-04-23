a0a76f90c26077cfebc3ab783ddbfa7643d68fe0
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:37:43 +0700

    add: KelasFactory.php

---
+++ database/factories/KelasFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Validation\Rules\Unique;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Kelas>
+ */
+class KelasFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'nama_kelas' => fake()->randomElement(['1A', '2A', '3A', '4A', '5A', '6A', '7A', '8A', '9A']),
+        ];
+    }
+}
