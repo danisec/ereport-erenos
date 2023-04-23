f577dc1ae57683290629d73e345e2a31f069de57
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:28:27 +0700

    update: view index

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function index() {
-        return view('pages.dashboard.index', [
+        return view('pages.dashboard.home.index', [
             'title' => 'Dashboard',
         ]);
     }
