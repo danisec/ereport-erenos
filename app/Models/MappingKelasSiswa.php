aca38e0f8e6a3ff5cff822859019ae95a94bc329
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:32:59 +0700

    add: Model MappingKelasSiswa.php

---
+++ app/Models/MappingKelasSiswa.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MappingKelasSiswa extends Model
+{
+    use HasFactory;
+
+    protected $table = 'mappingkelas_d';
+
+    protected $guarded= [
+        'idMappingKelas_D',
+    ];
+
+    public function siswa() 
+    {
+        return $this->belongsTo(Siswa::class, 'NIS', 'NIS');
+    }
+}
