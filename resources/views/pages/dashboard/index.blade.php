e198c0e3591bc4abbe87dfdd3cac91ead8624325
danisec <daniaprilyanto35@gmail.com>
2023-04-23 22:47:49 +0700

    move: views/pages/dashboard/home/index.blade.php

--- resources/views/pages/dashboard/index.blade.php
+++
@@ -1,9 +0,0 @@
-<x-layouts.app-layout title="{{ $title }}">
-
-    <x-organisms.header-dashboard />
-
-    <section class="layout mt-14">
-        <h2>Dashboard</h2>
-    </section>
-
-</x-layouts.app-layout>
