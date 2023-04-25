c30589b7bb2f8a09f0ec326f344b5843dc830037
danisec <daniaprilyanto35@gmail.com>
2023-04-25 14:58:38 +0700

    add: model MappingJadwal.php

---
+++ app/Models/MappingJadwal.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
+
+class MappingJadwal extends Model
+{
+    use HasFactory, Sortable;
+
+    protected $table = 'jadwal';
+
+    protected $guarded = ['idJadwal'];
+
+    public $sortable = [
+        'idThnAjaran',
+        'idKelas',
+        'NIP',
+        'hari',
+        'idPelajaran', 
+    ];
+
+    public function scopeFilter($query, array $filters)
+    {  
+       $query->when($filters['search'] ?? false, fn($query, $search) =>
+            $query->whereHas('kelas', fn($query) =>
+                $query->where('kelas', 'like', '%'.$search.'%')
+            )
+            ->orWhereHas('pelajaran', fn($query) =>
+                $query->where('nmPelajaran', 'like', '%'.$search.'%')
+            )
+            ->orWhereHas('guru', fn($query) =>
+                $query->where('namaGuru', 'like', '%'.$search.'%')
+            )
+            ->orWhere('hari', 'like', '%'.$search.'%')
+        );
+    }
+
+    public function kelas() 
+    {
+        return $this->belongsTo(Kelas::class, 'idKelas', 'idKelas');
+    }
+
+    public function tahunajaran() 
+    {
+        return $this->belongsTo(TahunAjaran::class, 'idThnAjaran', 'idThnAjaran');
+    }
+
+    public function guru() 
+    {
+        return $this->belongsTo(Guru::class, 'NIP', 'NIP');
+    }
+
+    public function pelajaran() 
+    {
+        return $this->belongsTo(Pelajaran::class, 'idPelajaran', 'idPelajaran');
+    }
+}
