63105989b284841773824c157bc6be6f08bc518b
danisec <daniaprilyanto35@gmail.com>
2023-04-25 14:57:15 +0700

    update: scroll horizontal nav

--- resources/views/components/organisms/header-dashboard.blade.php
+++ resources/views/components/organisms/header-dashboard.blade.php
@@ -1,6 +1,6 @@
     <nav class="h-28 w-full bg-login-pattern bg-cover bg-no-repeat">
-        <ul class="layout flex h-28 items-center justify-start">
-            <li class="flex flex-row items-center gap-12">
+        <ul class="layout flex h-28 items-center justify-start overflow-x-auto">
+            <li class="flex flex-row items-center gap-12 whitespace-nowrap">
                 @foreach ($navbar as $name => $url)
                     <a class="{{ request()->segment(2) == $url
                         ? 'text-white font-semibold text-xl leading-7 bg-nav-active/40 rounded-full py-2 px-5 transition-all ease-in'
