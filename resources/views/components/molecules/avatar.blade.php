3318c4411f32876ed9b94b9ba07cdba4b80d0ebb
danisec <daniaprilyanto35@gmail.com>
2023-05-07 01:32:55 +0700

    update: z-index

--- resources/views/components/molecules/avatar.blade.php
+++ resources/views/components/molecules/avatar.blade.php
@@ -3,7 +3,7 @@
         <img class="h-16 w-16 rounded-2xl" src="{{ URL('assets/icons/profile.svg') }}" alt="profile">
     </button>
 
-    <div class="absolute right-0 mr-14 mt-2 w-64 rounded-lg bg-white shadow-md shadow-slate-100" x-show="show"
+    <div class="absolute right-0 z-10 mr-14 mt-2 w-64 rounded-lg bg-white shadow-md shadow-slate-100" x-show="show"
         x-on:click.outside="show = !show" x-bind:class="hidden = !show"
         x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95"
         x-transition:enter-end="transform opacity-100 scale-100" x-cloak>
