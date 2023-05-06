2ff7133a1c09c8d9d05b846e2de3ec2435aee02e
danisec <daniaprilyanto35@gmail.com>
2023-05-07 01:39:50 +0700

    update: createsiswa.blade.php

--- resources/views/pages/dashboard/mappingkelas/createsiswa.blade.php
+++ resources/views/pages/dashboard/mappingkelas/createsiswa.blade.php
@@ -2,68 +2,106 @@
 
     <x-organisms.header-dashboard />
 
-    <section class="layout min-h-screen bg-hero py-14 font-rubik">
+    <section class="layout min-h-screen bg-hero py-6 font-rubik">
         <div class="w-full rounded-2xl bg-white py-5 shadow-sm">
 
             <div class="w-6/12 rounded-r-2xl bg-hero py-2">
                 <h4 class="text-gray-9000 px-11 text-2xl font-bold text-white">Menambah Data Siswa</h4>
             </div>
 
-            <form class="flex flex-col gap-6 px-11 pt-9" action="{{ route('mappingkelas.storesiswa') }}" method="post">
-                @csrf
+            <div class="mx-10 my-4 flex flex-row items-start gap-12">
 
-                <div class="flex flex-row gap-40">
+                <div class="flex flex-col gap-4">
+                    <div class="flex flex-col gap-1">
+                        <div class="flex flex-row items-center">
+                            <label class="mb-2 w-36 text-xl font-medium leading-9">Tahun Ajaran</label>
+                            <p class="mb-2 w-8 text-xl font-medium leading-9">:</p>
 
-                    <div class="flex flex-col gap-6">
-                        <input name="idMapping" type="hidden" value="{{ $idMapping->idMapping }}">
-                        <div class="flex flex-col gap-1">
-                            <div class="flex flex-row items-center">
-                                <label class="mb-2 w-64 text-xl font-medium leading-9">NIS</label>
-
-                                <select class="field-input-indigo w-96" id="nis" name="NIS" required>
-                                    <option selected disabled hidden>NIS</option>
-                                    @foreach ($nis as $item)
-                                        <option value="{{ $item->NIS }}">{{ $item->NIS }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            @error('NIS')
-                                <p class="invalid-feedback ml-64">
-                                    {{ $message }}
-                                </p>
-                            @enderror
-                        </div>
-
-                        <div class="flex flex-col gap-1">
-                            <div class="flex flex-row items-center gap-16">
-                                <label class="mb-2 w-48 text-xl font-medium leading-9">Nama</label>
-
-                                <select class="field-input-indigo w-96" id="nmSiswa" name="nmSiswa" required>
-                                    <option selected disabled hidden>Nama</option>
-                                    @foreach ($namasiswa as $item)
-                                        <option value="{{ $item->nmSiswa }}">{{ $item->nmSiswa }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-
-                            @error('nmSiswa')
-                                <p class="invalid-feedback ml-64">
-                                    {{ $message }}
-                                </p>
-                            @enderror
+                            <input class="field-input-gray w-52" name="idThnAjaran"
+                                value="{{ $mappingkelas->tahunajaran->thnAjaran }}" @disabled(true)
+                                @readonly(true)>
                         </div>
                     </div>
 
-                    <div class="mt-[4.4rem] flex flex-col gap-4">
+                    <div class="flex flex-col gap-1">
+                        <div class="flex flex-row items-center">
+                            <label class="mb-2 w-36 text-xl font-medium leading-9">Kelas</label>
+                            <p class="mb-2 w-8 text-xl font-medium leading-9">:</p>
+
+                            <input class="field-input-gray w-52" name="idKelas"
+                                value="{{ $mappingkelas->kelas->kelas }}" @disabled(true)
+                                @readonly(true)>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-[4.3rem] flex flex-col gap-4">
+                    <div class="flex flex-col gap-1">
+                        <div class="flex flex-row items-center">
+                            <label class="mb-2 w-28 text-xl font-medium leading-9">Wali Kelas</label>
+                            <p class="mb-2 w-8 text-xl font-medium leading-9">:</p>
+
+                            <input class="field-input-gray w-52" name="NIP"
+                                value="  {{ $mappingkelas->guru->namaGuru }}" @disabled(true)
+                                @readonly(true)>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <form class="mt-4 mb-2 ml-32 flex flex-col gap-6 px-11" action="{{ route('mappingkelas.storesiswa') }}"
+                method="post">
+                @csrf
+
+                <div class="flex flex-row gap-12">
+                    <input name="idMapping" type="hidden" value="{{ $idMapping->idMapping }}" value="">
+                    <div class="ml-1 flex flex-col items-center gap-1">
+                        <div class="flex flex-row items-center">
+                            <label class="mb-2 w-14 text-xl font-medium leading-9">NIS</label>
+
+                            <select class="field-input-indigo w-40" id="nis" name="NIS" required>
+                                <option selected disabled hidden>NIS</option>
+                                @foreach ($nis as $item)
+                                    <option value="{{ $item->NIS }}">{{ $item->NIS }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        @error('NIS')
+                            <p class="invalid-feedback ml-14">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+
+                    <div class="flex flex-col">
+                        <div class="flex flex-row items-center gap-16">
+                            <label class="mb-2 w-4 text-xl font-medium leading-9">Nama</label>
+
+                            <select class="field-input-indigo w-56" id="nmSiswa" name="nmSiswa" required>
+                                <option selected disabled hidden>Nama</option>
+                                @foreach ($namasiswa as $item)
+                                    <option value="{{ $item->nmSiswa }}">{{ $item->nmSiswa }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        @error('nmSiswa')
+                            <p class="invalid-feedback ml-64">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+
+                    <div class="ml-28 flex flex-col">
                         <button
-                            class="flex flex-row items-center gap-2 rounded-md bg-cyan-300 px-4 py-2.5 shadow-xl shadow-gray-300"
+                            class="flex flex-row items-center gap-2 rounded-md bg-cyan-300 px-2 py-0 shadow-lg shadow-gray-300"
                             type="submit">
                             <p class="text-center text-xl font-normal">Tambah</p>
                             <x-atoms.plus :alt="'tambah-datasiswa'" />
                         </button>
                     </div>
-
                 </div>
 
             </form>
@@ -106,16 +144,16 @@
                         @foreach ($mappingkelasd as $item)
                             <tbody>
                                 <tr class="bg-white text-base font-medium leading-5 hover:bg-gray-50">
-                                    <th class="py-4 px-9" scope="row">
+                                    <th class="px-9" scope="row">
                                         {{ ($mappingkelasd->currentPage() - 1) * $mappingkelasd->perPage() + $loop->iteration }}
                                     </th>
-                                    <td class="py-4 pl-6">
+                                    <td class="pl-6">
                                         {{ $item->siswa->NIS }}
                                     </td>
-                                    <td class="py-4 pl-6">
+                                    <td class="pl-6">
                                         {{ $item->siswa->nmSiswa }}
                                     </td>
-                                    <td class="py-4">
+                                    <td>
                                         <div x-data="{ show: false }">
                                             <button class="focus:outline-none" type="button" @click="show = !show">
                                                 <x-atoms.trash :alt="'delete-siswa'" />
@@ -135,8 +173,19 @@
             </div>
 
             <div class="mt-6 flex flex-row items-center justify-center gap-40">
-                <a class="rounded-sm bg-cyan-300 px-10 py-3 text-center text-sm font-normal shadow-xl shadow-gray-300"
-                    href="{{ URL('dashboard/mappingkelas') }}">Selesai</a>
+                @if ($mappingkelasd->where('idMapping', $idMapping->idMapping)->count() == 0)
+                    <form action="{{ route('mappingkelas.destroykelasid', $idMapping->idMapping) }}" method="post">
+                        @method('delete')
+                        @csrf
+
+                        <button
+                            class="rounded-sm bg-cyan-300 px-10 py-3 text-center text-sm font-normal shadow-xl shadow-gray-300"
+                            type="submit">Selesai</button>
+                    </form>
+                @else
+                    <a class="rounded-sm bg-cyan-300 px-10 py-3 text-center text-sm font-normal shadow-xl shadow-gray-300"
+                        href="{{ URL('dashboard/mappingkelas') }}">Selesai</a>
+                @endif
             </div>
 
         </div>
