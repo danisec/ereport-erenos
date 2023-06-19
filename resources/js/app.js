d71f9e512023ca2fb1a6c444f4e0ccf43da37bbf
danisec <daniaprilyanto35@gmail.com>
2023-06-19 16:35:07 +0700

    update: app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,22 @@ import "./bootstrap";
 import Alpine from "alpinejs";
 import "./mappingkelas";
 
+if (window.location.pathname === "/dashboard/presensi/tambah-presensi") {
+    import("./presensi")
+        .then((module) => {})
+        .catch((error) => {
+            console.error("Gagal mengimpor ./presensi:", error);
+        });
+}
+
+if (window.location.pathname === "/dashboard/presensi") {
+    import("./filterSearchPresensi")
+        .then((module) => {})
+        .catch((error) => {
+            console.error("Gagal mengimpor ./filterSearchPresensi:", error);
+        });
+}
+
 window.Alpine = Alpine;
 
 Alpine.start();
