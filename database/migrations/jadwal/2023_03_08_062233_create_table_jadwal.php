448b1bc95524c7fe3ee176e8aba8ae6aad00ed84
danisec <daniaprilyanto35@gmail.com>
2023-06-27 23:38:55 +0700

    add: migrations/jadwal

---
+++ database/migrations/jadwal/2023_03_08_062233_create_table_jadwal.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jadwal', function (Blueprint $table) {
+            $table->id('idJadwal');
+            $table->bigInteger('idKelas')->unsigned();
+            $table->bigInteger('idThnAjaran')->unsigned();
+            $table->char('NIP', 10);
+            $table->enum('hari', [
+                'senin',
+                'selasa',
+                'rabu',
+                'kamis',
+                'jumat',
+                'sabtu',
+            ]);
+            $table->bigInteger('idPelajaran')->unsigned();
+            $table->time('mulai');
+            $table->time('selesai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('jadwal', function (Blueprint $table) {
+            Schema::dropIfExists('jadwal');
+        });
+    }
+};
