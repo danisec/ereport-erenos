e5d82c06779cf8ce0ce6a6b42822baccfc37f267
danisec <daniaprilyanto35@gmail.com>
2023-06-27 23:41:48 +0700

    update: app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,9 @@
 import "./bootstrap";
+import jQuery from "jquery";
 import Alpine from "alpinejs";
 import "./mappingkelas";
 
-if (window.location.pathname === "/dashboard/presensi/tambah-presensi") {
+if (window.location.pathname.includes("/dashboard/presensi/")) {
     import("./presensi")
         .then((module) => {})
         .catch((error) => {
@@ -18,9 +19,42 @@ if (window.location.pathname === "/dashboard/presensi") {
         });
 }
 
+if (window.location.pathname === "/dashboard/materi/tambah-materi") {
+    import("./tabelMateri")
+        .then((module) => {})
+        .catch((error) => {
+            console.error("Gagal mengimpor ./tabelMateri:", error);
+        });
+}
+
+if (
+    window.location.pathname.includes("/dashboard/materi/") ||
+    window.location.pathname.includes("/dashboard/pelajaran/")
+) {
+    import("./textareaMateri")
+        .then((module) => {})
+        .catch((error) => {
+            console.error("Gagal mengimpor ./textareaMateri:", error);
+        });
+}
+
+if (window.location.pathname === "/dashboard/nilai/tambah-nilai") {
+    import("./date")
+        .then((module) => {})
+        .catch((error) => {
+            console.error("Gagal mengimpor ./date:", error);
+        });
+}
+
+if (window.location.pathname.includes("/dashboard/nilai/")) {
+    import("./nilai")
+        .then((module) => {})
+        .catch((error) => {
+            console.error("Gagal mengimpor ./nilai:", error);
+        });
+}
+
+window.$ = jQuery;
 window.Alpine = Alpine;
 
 Alpine.start();
-
-import jQuery from "jquery";
-window.$ = jQuery;
