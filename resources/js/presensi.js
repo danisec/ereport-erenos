91622ebe0d65354f94437a38fddf17d4651976fe
danisec <daniaprilyanto35@gmail.com>
2023-06-27 23:42:16 +0700

    add: presensi.js

--- resources/js/presensi.js
+++ resources/js/presensi.js
@@ -216,3 +216,36 @@ function showSiswaTable(siswaData) {
         siswaTableBody.append(row);
     });
 }
+
+function datetime() {
+    const dateInput = document.getElementById("datetime");
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    const currentDate = `${year}-${month < 10 ? `0${month}` : month}-${
+        day < 10 ? `0${day}` : day
+    }T${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+    }`;
+
+    dateInput.value = currentDate;
+}
+
+datetime();
+
+function hideScrollTopButton() {
+    const scrollTopButton = document.getElementById("scroll-button");
+
+    if (window.pageYOffset < 200) {
+        scrollTopButton.style.display = "none";
+    } else {
+        scrollTopButton.style.display = "block";
+    }
+}
+
+window.addEventListener("scroll", hideScrollTopButton);
