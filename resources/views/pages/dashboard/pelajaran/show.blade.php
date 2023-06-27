aa7008bae7a7823dfec1705f0a4673c891dd413e
danisec <daniaprilyanto35@gmail.com>
2023-06-27 23:46:20 +0700

    update: pelajaran/show.blade.php

--- resources/views/pages/dashboard/pelajaran/show.blade.php
+++ resources/views/pages/dashboard/pelajaran/show.blade.php
@@ -13,33 +13,69 @@
 
                 <div class="flex flex-col gap-1">
                     <div class="flex flex-row items-center">
-                        <label class="mb-2 w-64 text-xl font-medium leading-9">Kode Pelajaran</label>
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Kode Pelajaran</label>
                         <input class="field-input-gray w-52" value="{{ $pelajaran->kodePelajaran }}"
                             @disabled(true) @readonly(true)>
                     </div>
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <div class="flex flex-row items-center gap-16">
-                        <label class="mb-2 w-48 text-xl font-medium leading-9">Nama Pelajaran</label>
+                    <div class="flex flex-row items-center">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Nama Pelajaran</label>
                         <input class="field-input-gray w-8/12" value="{{ $pelajaran->nmPelajaran }}"
                             @disabled(true) @readonly(true)>
                     </div>
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <div class="flex flex-row items-center gap-16">
-                        <label class="mb-2 w-48 text-xl font-medium leading-9">Singkatan</label>
+                    <div class="flex flex-row items-center">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Singkatan</label>
                         <input class="field-input-gray w-96" value="{{ $pelajaran->nmSingkatan }}"
                             @disabled(true) @readonly(true)>
                     </div>
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <div class="flex flex-row items-center gap-16">
-                        <label class="mb-2 w-48 text-xl font-medium leading-9">Nilai KKM</label>
+                    <div class="flex flex-row items-center">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Nilai KKM</label>
                         <input class="field-input-gray w-52" value="{{ $pelajaran->KKM }}" @disabled(true)
                             @readonly(true)>
+                    </div>
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi A</label>
+
+                        <textarea class="field-input-gray materiTextarea w-[50rem]" name="deskripsiA" placeholder="Deskripsi A"
+                            @disabled(true) @readonly(true)>{{ $pelajaran->deskripsiA }}</textarea>
+                    </div>
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi B</label>
+
+                        <textarea class="field-input-gray materiTextarea w-[50rem]" name="deskripsiB" placeholder="Deskripsi B"
+                            @disabled(true) @readonly(true)>{{ $pelajaran->deskripsiB }}</textarea>
+                    </div>
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi C</label>
+
+                        <textarea class="field-input-gray materiTextarea w-[50rem]" name="deskripsiC" placeholder="Deskripsi C"
+                            @disabled(true) @readonly(true)>{{ $pelajaran->deskripsiC }}</textarea>
+                    </div>
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi D</label>
+
+                        <textarea class="field-input-gray materiTextarea w-[50rem]" name="deskripsiD" placeholder="Deskripsi D"
+                            @disabled(true) @readonly(true)>{{ $pelajaran->deskripsiD }}</textarea>
                     </div>
                 </div>
 
