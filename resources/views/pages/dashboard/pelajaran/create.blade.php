f88d60c6349dfe7862b658a3213f71226a0d779e
danisec <daniaprilyanto35@gmail.com>
2023-06-27 23:45:57 +0700

    update: pelajaran/create.blade.php

--- resources/views/pages/dashboard/pelajaran/create.blade.php
+++ resources/views/pages/dashboard/pelajaran/create.blade.php
@@ -14,55 +14,111 @@
 
                 <div class="flex flex-col gap-1">
                     <div class="flex flex-row items-center">
-                        <label class="mb-2 w-64 text-xl font-medium leading-9">Kode Pelajaran</label>
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Kode Pelajaran</label>
                         <input class="field-input-indigo w-52" name="kodePelajaran" type="number"
                             value="{{ old('kodePelajaran') }}" maxlength="8" placeholder="Nomor" required>
                     </div>
 
                     @error('kodePelajaran')
-                        <p class="invalid-feedback ml-64">
+                        <p class="invalid-feedback ml-52">
                             {{ $message }}
                         </p>
                     @enderror
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <div class="flex flex-row items-center gap-16">
-                        <label class="mb-2 w-48 text-xl font-medium leading-9">Nama Pelajaran</label>
+                    <div class="flex flex-row items-center">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Nama Pelajaran</label>
                         <input class="field-input-indigo w-8/12" name="nmPelajaran" type="text"
                             value="{{ old('nmPelajaran') }}" placeholder="Nama" required>
                     </div>
 
                     @error('nmPelajaran')
-                        <p class="invalid-feedback ml-64">
+                        <p class="invalid-feedback ml-52">
                             {{ $message }}
                         </p>
                     @enderror
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <div class="flex flex-row items-center gap-16">
-                        <label class="mb-2 w-48 text-xl font-medium leading-9">Singkatan</label>
+                    <div class="flex flex-row items-center">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Singkatan</label>
                         <input class="field-input-indigo w-96" name="nmSingkatan" type="text"
                             value="{{ old('nmSingkatan') }}" placeholder="Nama Singkatan" required>
                     </div>
 
                     @error('nmSingkatan')
-                        <p class="invalid-feedback ml-64">
+                        <p class="invalid-feedback ml-52">
                             {{ $message }}
                         </p>
                     @enderror
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <div class="flex flex-row items-center gap-16">
-                        <label class="mb-2 w-48 text-xl font-medium leading-9">Nilai KKM</label>
+                    <div class="flex flex-row items-center">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Nilai KKM</label>
                         <input class="field-input-indigo w-52" name="KKM" type="number" value="{{ old('KKM') }}"
                             maxlength="3" placeholder="Nilai KKM" required>
                     </div>
 
                     @error('KKM')
-                        <p class="invalid-feedback ml-64">
+                        <p class="invalid-feedback ml-52">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi A</label>
+
+                        <textarea class="field-input-indigo w-[50rem]" name="deskripsiA" rows="4" placeholder="Deskripsi A" required></textarea>
+                    </div>
+
+                    @error('deskripsiA')
+                        <p class="invalid-feedback ml-52">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi B</label>
+
+                        <textarea class="field-input-indigo w-[50rem]" name="deskripsiB" rows="4" placeholder="Deskripsi B" required></textarea>
+                    </div>
+
+                    @error('deskripsiB')
+                        <p class="invalid-feedback ml-52">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi C</label>
+
+                        <textarea class="field-input-indigo w-[50rem]" name="deskripsiC" rows="4" placeholder="Deskripsi C" required></textarea>
+                    </div>
+
+                    @error('deskripsiC')
+                        <p class="invalid-feedback ml-52">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi D</label>
+
+                        <textarea class="field-input-indigo w-[50rem]" name="deskripsiD" rows="4" placeholder="Deskripsi D" required></textarea>
+                    </div>
+
+                    @error('deskripsiD')
+                        <p class="invalid-feedback ml-52">
                             {{ $message }}
                         </p>
                     @enderror
