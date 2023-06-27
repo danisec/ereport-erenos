ca1dfca11ca14fb20f269b4c3fac7943ffcc28dc
danisec <daniaprilyanto35@gmail.com>
2023-06-27 23:46:09 +0700

    update: pelajaran/edit.blade.php

--- resources/views/pages/dashboard/pelajaran/edit.blade.php
+++ resources/views/pages/dashboard/pelajaran/edit.blade.php
@@ -16,7 +16,7 @@
 
                 <div class="flex flex-col gap-1">
                     <div class="flex flex-row items-center">
-                        <label class="mb-2 w-64 text-xl font-medium leading-9">Kode Pelajaran</label>
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Kode Pelajaran</label>
                         <input
                             class="@error('kodePelajaran') border-red-300 bg-red-300 @enderror field-input-indigo w-52"
                             name="kodePelajaran" type="number" value="{{ $pelajaran->kodePelajaran }}" maxlength="8"
@@ -31,8 +31,8 @@
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <div class="flex flex-row items-center gap-16">
-                        <label class="mb-2 w-48 text-xl font-medium leading-9">Nama Pelajaran</label>
+                    <div class="flex flex-row items-center">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Nama Pelajaran</label>
                         <input
                             class="@error('nmPelajaran') border-red-300 bg-red-300 @enderror field-input-indigo w-8/12"
                             name="nmPelajaran" type="text" value="{{ $pelajaran->nmPelajaran }}" placeholder="Nama"
@@ -47,8 +47,8 @@
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <div class="flex flex-row items-center gap-16">
-                        <label class="mb-2 w-48 text-xl font-medium leading-9">Singkatan</label>
+                    <div class="flex flex-row items-center">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Singkatan</label>
                         <input class="@error('nmSingkatan') border-red-300 bg-red-300 @enderror field-input-indigo w-96"
                             name="nmSingkatan" type="text" value="{{ $pelajaran->nmSingkatan }}"
                             placeholder="Nama Singkatan" required>
@@ -62,8 +62,8 @@
                 </div>
 
                 <div class="flex flex-col gap-1">
-                    <div class="flex flex-row items-center gap-16">
-                        <label class="mb-2 w-48 text-xl font-medium leading-9">Nilai KKM</label>
+                    <div class="flex flex-row items-center">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Nilai KKM</label>
                         <input class="@error('KKM') border-red-300 bg-red-300 @enderror field-input-indigo w-52"
                             name="KKM" type="number" value="{{ $pelajaran->KKM }}" maxlength="3"
                             placeholder="Nilai KKM" required>
@@ -71,6 +71,66 @@
 
                     @error('KKM')
                         <p class="invalid-feedback ml-64">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi A</label>
+
+                        <textarea class="@error('deskripsiA') border-red-300 bg-red-300 @enderror materiTextarea field-input-indigo w-[50rem]"
+                            name="deskripsiA" placeholder="Deskripsi A">{{ $pelajaran->deskripsiA }}</textarea>
+                    </div>
+
+                    @error('deskripsiA')
+                        <p class="invalid-feedback ml-32">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi B</label>
+
+                        <textarea class="@error('deskripsiB') border-red-300 bg-red-300 @enderror materiTextarea field-input-indigo w-[50rem]"
+                            name="deskripsiB" placeholder="Deskripsi A">{{ $pelajaran->deskripsiB }}</textarea>
+                    </div>
+
+                    @error('deskripsiB')
+                        <p class="invalid-feedback ml-32">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi C</label>
+
+                        <textarea class="@error('deskripsiC') border-red-300 bg-red-300 @enderror materiTextarea field-input-indigo w-[50rem]"
+                            name="deskripsiC" placeholder="Deskripsi A">{{ $pelajaran->deskripsiC }}</textarea>
+                    </div>
+
+                    @error('deskripsiC')
+                        <p class="invalid-feedback ml-32">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+
+                <div class="flex flex-col gap-1">
+                    <div class="flex flex-row items-start">
+                        <label class="mb-2 w-52 text-xl font-medium leading-9">Deskripsi D</label>
+
+                        <textarea class="@error('deskripsiD') border-red-300 bg-red-300 @enderror materiTextarea field-input-indigo w-[50rem]"
+                            name="deskripsiD" placeholder="Deskripsi D">{{ $pelajaran->deskripsiD }}</textarea>
+                    </div>
+
+                    @error('deskripsiD')
+                        <p class="invalid-feedback ml-32">
                             {{ $message }}
                         </p>
                     @enderror
