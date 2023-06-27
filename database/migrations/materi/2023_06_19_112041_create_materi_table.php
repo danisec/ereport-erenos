b3bf5d76ed928f95a45f83c421d3a3b50427321b
danisec <daniaprilyanto35@gmail.com>
2023-06-27 23:40:26 +0700

    add: migrations/materi

---
+++ database/migrations/materi/2023_06_19_112041_create_materi_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materi', function (Blueprint $table) {
+            $table->id('idMateri');
+            $table->string('materi', 1000);
+            $table->bigInteger('idPelajaran')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materi');
+    }
+};
