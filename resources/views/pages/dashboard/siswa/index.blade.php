af62dd3945d8c272a143b15dd78b9c9d6a66cfb9
danisec <daniaprilyanto35@gmail.com>
2023-06-27 23:47:10 +0700

    update: siswa/index.blade.php

--- resources/views/pages/dashboard/siswa/index.blade.php
+++ resources/views/pages/dashboard/siswa/index.blade.php
@@ -70,9 +70,10 @@
                 </thead>
 
                 @if ($siswa->count())
-                    @foreach ($siswa as $item)
+                    @foreach ($siswa as $index => $item)
                         <tbody>
-                            <tr class="bg-white text-base font-medium leading-5 hover:bg-gray-50">
+                            <tr
+                                class="{{ $index % 2 === 0 ? 'bg-white' : 'bg-blue-100' }} text-base font-medium leading-5">
                                 <th class="px-9" scope="row">
                                     {{ ($siswa->currentPage() - 1) * $siswa->perPage() + $loop->iteration }}
                                 </th>
