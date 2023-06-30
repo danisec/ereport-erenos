135d487a0d567c2a72e81bb0a040b7ef48b59540
danisec <daniaprilyanto35@gmail.com>
2023-06-30 13:43:17 +0700

    add: kelola-akun/create.blade.php

---
+++ resources/views/pages/dashboard/kelola-akun/create.blade.php
@@ -0,0 +1,97 @@
+<x-layouts.app-layout title="{{ $title }}">
+
+    <div class="h-32 w-full bg-login-pattern bg-cover bg-no-repeat">
+        <x-molecules.breadcrumb :title="$title . ' ' . 'to the site'" />
+    </div>
+
+    <div class="absolute min-h-screen w-full bg-hero">
+        <div class="relative inset-x-0 -top-20 mx-auto w-5/12 rounded-3xl bg-white py-16 shadow-xl">
+            <div class="flex flex-col items-center justify-center gap-11">
+                <x-atoms.logo classLogo="h-auto w-48" />
+                <p class="text-center font-lexend text-2xl font-normal leading-7">Daftar akun Anda</p>
+            </div>
+
+            <form class="mt-14" action="{{ route('kelolaAkun.store') }}" method="post">
+                @csrf
+
+                <div class="flex flex-col justify-center gap-12">
+                    <div>
+                        <input class="@error('name') border-red-500 bg-red-500 @enderror field-input" name="name"
+                            type="text" value="{{ old('name') }}" autofocus placeholder="Nama Lengkap" required>
+
+                        @error('name')
+                            <p class="invalid-feedback">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <input class="@error('username') border-red-500 bg-red-500 @enderror field-input"
+                            name="username" type="text" value="{{ old('username') }}" placeholder="Username"
+                            required>
+
+                        @error('username')
+                            <p class="invalid-feedback">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <input class="@error('email') border-red-500 bg-red-500 @enderror field-input" name="email"
+                            type="email" value="{{ old('email') }}" placeholder="Email" required>
+
+                        @error('email')
+                            <p class="invalid-feedback">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <input class="@error('NIP') border-red-500 bg-red-500 @enderror field-input" name="NIP"
+                            type="number" value="{{ old('NIP') }}" placeholder="NIP" required>
+
+                        @error('NIP')
+                            <p class="invalid-feedback">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <select class="@error('role') border-red-500 bg-red-500 @enderror field-input" name="role"
+                            required>
+                            <option selected disabled hidden>Role</option>
+
+                            @foreach ($role as $index => $item)
+                                <option value="{{ $index }}">{{ $item }}</option>
+                            @endforeach
+                        </select>
+
+                        @error('role')
+                            <p class="invalid-feedback">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+
+                    <div>
+                        <input class="@error('password') border-red-500 bg-red-500 @enderror field-input"
+                            name="password" type="password" placeholder="Password" required>
+
+                        @error('password')
+                            <p class="mx-auto w-9/12 text-sm font-medium tracking-wide text-red-500">
+                                {{ $message }}
+                            </p>
+                        @enderror
+                    </div>
+
+                    <button class="btn-auth" type="submit">Register</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</x-layouts.app-layout>
