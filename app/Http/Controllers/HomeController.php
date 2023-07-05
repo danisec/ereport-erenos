099ed6d9798672fe59fd29220b71b1b9a8fca080
danisec <daniaprilyanto35@gmail.com>
2023-07-05 10:12:22 +0700

    update: HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pengumuman;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $pengumumanLatest = Pengumuman::latest('idPengumuman')->first();
+
         return view('pages.home.index', [
             'title' => 'Home',
+            'pengumuman' => Pengumuman::orderBy('idPengumuman', 'desc')->get(),
+            'pengumumanLatest' => $pengumumanLatest,
         ]);
     }
 }
