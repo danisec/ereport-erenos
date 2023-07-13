4113112dd18c70e3345e97226c8d97b4bbabbf36
danisec <daniaprilyanto35@gmail.com>
2023-07-13 09:36:33 +0700

    refactor: controllers/KelolaAkunController.php

--- app/Http/Controllers/KelolaAkunController.php
+++ app/Http/Controllers/KelolaAkunController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\KelolaAkun;
 use App\Models\User;
+use App\Services\KelolaAkunService;
 use Illuminate\Http\Request;
 
 class KelolaAkunController extends Controller
 {
+    public function __construct(KelolaAkunService $kelolaAkunService)
+    {
+        $this->kelolaAkunService = $kelolaAkunService;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -45,37 +51,11 @@ class KelolaAkunController extends Controller
      */
     public function store(Request $request)
     {
-        {
-            $validatedData = $request->validate([
-                'name' => 'required|max:255',
-                'username' => ['required', 'min:4', 'max:255', 'unique:users'],
-                'email' => 'required|email:dns|unique:users',
-                'NIP' => 'required|unique:users',
-                'password' => 'required|min:8|max:255',
-            ], [
-                'name.required' => 'Nama Lengkap harus diisi',
-                'name.max' => 'Nama Lengkap maksimal 255 karakter',
-                'username.min' => 'Username minimal 4 karakter',
-                'username.max' => 'Username maksimal 255 karakter',
-                'username.unique' => 'Username sudah terdaftar',
-                'email.required' => 'Email harus diisi',
-                'email.email' => 'Email tidak valid',
-                'email.unique' => 'Email sudah terdaftar',
-                'NIP.required' => 'NIP harus diisi',
-                'NIP.unique' => 'NIP sudah terdaftar',
-                'password.required' => 'Password harus diisi',
-                'password.min' => 'Password minimal 8 karakter',
-                'password.max' => 'Password maksimal 255 karakter',
-            ]);
+        $this->kelolaAkunService->storeKelolaAkun($request);
 
-            $validatedData['password'] = bcrypt($validatedData['password']);
-            
-            User::create($validatedData);
+        $notif = notify()->success('Pendaftaran akun berhasil');
 
-            $notif = notify()->success('Pendaftaran akun berhasil');
-
-            return redirect('/dashboard/kelola-akun')->with('notif', $notif);
-        }
+        return redirect('/dashboard/kelola-akun')->with('notif', $notif);
     }
 
     /**
@@ -100,9 +80,6 @@ class KelolaAkunController extends Controller
      */
     public function edit($id)
     {
-        // $akun = User::where('id', $id)->first();
-        // dd($akun);
-
         return view('pages.dashboard.kelola-akun.edit', [
             'title' => 'Ubah Akun',
             'akun' => User::where('id', $id)->first(),
@@ -122,24 +99,7 @@ class KelolaAkunController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $validatedData = $request->validate([
-            'name' => 'max:255',
-            'username' => ['required', 'min:4', 'max:255'],
-            'email' => 'email:dns',
-            'NIP' => '',
-            'password' => 'min:8|max:255',
-        ], [
-            'name.max' => 'Nama Lengkap maksimal 255 karakter',
-            'username.min' => 'Username minimal 4 karakter',
-            'username.max' => 'Username maksimal 255 karakter',
-            'email.email' => 'Email tidak valid',
-            'password.min' => 'Password minimal 8 karakter',
-            'password.max' => 'Password maksimal 255 karakter',
-        ]);
-
-        $validatedData['password'] = bcrypt($validatedData['password']);
-
-        User::where('id', $id)->update($validatedData);
+        $this->kelolaAkunService->updateKelolaAkun($request, $id);
 
         $notif = notify()->success('Data Akun Berhasil Diubah');
 
