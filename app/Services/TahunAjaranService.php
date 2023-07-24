f4ef4167e2b27f35912a602312a526352fc2204c
danisec <daniaprilyanto35@gmail.com>
2023-07-25 01:43:13 +0700

    update: services/TahunAjaranService.php

--- app/Services/TahunAjaranService.php
+++ app/Services/TahunAjaranService.php
@@ -2,44 +2,101 @@
 
 namespace App\Services;
 
+use App\Models\Semester;
 use App\Models\TahunAjaran;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TahunAjaranService
 {
     public function storeTahunAjaran(Request $request)
     {
         $validatedData = $request->validate([
-            'thnAjaran' => 'required|max:15|unique:tahun_ajaran,thnAjaran,NULL,id,semester,' . $request->semester,
-            'semester' => 'required|in:Gasal,Genap,Pertengahan Tengah Semester 1,Pertengahan Akhir Semester 1,Pertengahan Tengah Semester 2,Pertengahan Akhir Semester 2',
+            'thnAjaran' => 'required|max:15|unique:tahun_ajaran,thnAjaran',
         ], [
             'thnAjaran.required' => 'Tahun ajaran harus diisi',
             'thnAjaran.max' => 'Tahun ajaran maksimal 15 karakter',
             'thnAjaran.unique' => 'Tahun ajaran sudah terdaftar',
-            'semester.required' => 'Semester harus diisi',
-            'semester.in' => 'Semester harus berupa Gasal atau Genap',
         ]);
 
-        $tahunAjaran = TahunAjaran::create($validatedData);
+        DB::beginTransaction();
 
-        return $tahunAjaran;
+        try {
+            TahunAjaran::create($validatedData);
+
+            DB::commit();
+            
+
+            return redirect('/dashboard/tahunajaran/tambah-tahunajaran/semester');
+        } catch (\Exception $e) {
+            // Rollback transaksi jika terjadi kesalahan
+            DB::rollback();
+
+            $error = $e->getMessage();
+            $notif = notify()->error('Terjadi kesalahan saat menyimpan data: ' . $error);
+
+            return back()->with('notif', $notif);
+        }
+    }
+
+    public function storeSemester(Request $request)
+    {
+        $validatedSemester = $request->validate([
+            'idThnAjaran' => 'required',
+            'semester' => 'required',
+        ], [
+            'idThnAjaran.required' => 'Tahun Ajaran harus diisi',
+            'idThnAjaran.unique' => 'Tahun Ajaran sudah terdaftar',
+            'semester.required' => 'Semester harus diisi',
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            Semester::create($validatedSemester);
+
+            DB::commit();
+
+            $notif = notify()->success('Data Tahun Ajaran Berhasil Ditambahkan');
+
+            return back()->with('notif', $notif);
+        } catch (\Exception $e) {
+            // Rollback transaksi jika terjadi kesalahan
+            DB::rollback();
+
+            $error = $e->getMessage();
+            $notif = notify()->error('Terjadi kesalahan saat menyimpan data: ' . $error);
+
+            return back();
+        }
     }
 
     public function updateTahunAjaran(Request $request, $id)
     {
         $validatedData = $request->validate([
-            'thnAjaran' => 'required|max:15|unique:tahun_ajaran,thnAjaran,NULL,id,semester,' . $request->semester,
-            'semester' => 'required|in:Gasal,Genap,Pertengahan Tengah Semester 1,Pertengahan Akhir Semester 1,Pertengahan Tengah Semester 2,Pertengahan Akhir Semester 2',
+            'semester' => 'required',
         ], [
-            'thnAjaran.required' => 'Tahun ajaran harus diisi',
-            'thnAjaran.max' => 'Tahun ajaran maksimal 15 karakter',
-            'thnAjaran.unique' => 'Tahun ajaran sudah terdaftar',
             'semester.required' => 'Semester harus diisi',
-            'semester.in' => 'Semester harus berupa Gasal atau Genap',
         ]);
 
-        $tahunAjaran = TahunAjaran::where('idThnAjaran', $id)->update($validatedData);
+        DB::beginTransaction();
 
-        return $tahunAjaran;
+        try {
+            Semester::where('idSemester', $id)->update($validatedData);
+
+            DB::commit();
+
+            $notif = notify()->success('Data Tahun Ajaran Berhasil Diubah');
+
+            return redirect('/dashboard/tahunajaran')->with('notif', $notif);
+        } catch (\Exception $e) {
+            // Rollback transaksi jika terjadi kesalahan
+            DB::rollback();
+
+            $error = $e->getMessage();
+            $notif = notify()->error('Terjadi kesalahan saat menyimpan data: ' . $error);
+
+            return back();
+        }
     }
 }
