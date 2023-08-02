2f87e5a835cf76de56c4550964ce6aec64c11072
danisec <daniaprilyanto35@gmail.com>
2023-08-03 02:52:24 +0700

    add: models/Rapor.php

---
+++ app/Models/Rapor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rapor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'rapor';
+
+    protected $primaryKey = 'idRapor';
+    protected $guarded = ['idRapor'];
+
+    public function siswa() 
+    {
+        return $this->belongsTo(Siswa::class, 'NIS', 'NIS');
+    }
+
+    public function kelas() 
+    {
+        return $this->belongsTo(Kelas::class, 'idKelas', 'idKelas');
+    }
+
+    public function semester() 
+    {
+        return $this->belongsTo(Semester::class, 'idSemester', 'idSemester');
+    }
+
+    public function rapor_d()
+    {
+        return $this->hasMany(Rapor_D::class, 'idRapor', 'idRapor');
+    }
+}
