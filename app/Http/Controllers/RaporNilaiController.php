49ce0be9b85360cfc44acfa31da5579f0901e1da
danisec <daniaprilyanto35@gmail.com>
2023-08-07 17:31:21 +0700

    add: controllers/RaporNilaiController

---
+++ app/Http/Controllers/RaporNilaiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RaporNilai;
+use Illuminate\Http\Request;
+
+class RaporNilaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\RaporNilai  $raporNilai
+     * @return \Illuminate\Http\Response
+     */
+    public function show(RaporNilai $raporNilai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\RaporNilai  $raporNilai
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(RaporNilai $raporNilai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\RaporNilai  $raporNilai
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, RaporNilai $raporNilai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\RaporNilai  $raporNilai
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(RaporNilai $raporNilai)
+    {
+        //
+    }
+}
