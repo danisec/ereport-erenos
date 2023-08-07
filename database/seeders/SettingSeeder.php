71706ae4679f69050de84190b9e8e267493c735d
danisec <daniaprilyanto35@gmail.com>
2023-08-08 04:14:17 +0700

    add: seeders/SettingSeeder

---
+++ database/seeders/SettingSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Setting;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Setting::truncate();
+
+        Setting::create([
+            'KepSek' => 'Lusiana Sele, S.Pd.'
+        ]);
+    }
+}
