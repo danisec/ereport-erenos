2bbcb75ec814fdaac758cc68552d7e16d2e994d8
danisec <daniaprilyanto35@gmail.com>
2023-09-05 20:21:55 +0700

    add: history-siswa/index.blade.php

---
+++ resources/views/pages/dashboard/history-siswa/index.blade.php
@@ -0,0 +1,116 @@
+<x-layouts.app-layout title="{{ $title }}">
+
+    <x-organisms.header-dashboard />
+
+    <section class="layout min-h-screen bg-hero py-6 font-rubik">
+
+        <div class="relative overflow-x-auto rounded-2xl shadow-sm">
+            <table class="w-full text-left">
+
+                <div class="bg-white px-9 py-5">
+                    <div class="flex h-14 flex-row items-center justify-between rounded-3xl bg-hero px-20">
+                        <h4 class="text-2xl font-bold text-white">Data History Siswa</h4>
+
+                        <div class="flex flex-row gap-4">
+                            <form class="my-4" action="" method="GET">
+                                @csrf
+
+                                @if (request('nmSiswa'))
+                                    <input name="nmSiswa" type="hidden" value="{{ request('nmSiswa') }}">
+                                @endif
+
+                                <x-molecules.search :placeholder="'Cari Siswa'" />
+                            </form>
+
+                            <a href="{{ route('historysiswa.create') }}">
+                                <x-atoms.plus :alt="'tambah-historysiswa'" />
+                            </a>
+
+                        </div>
+                    </div>
+                </div>
+
+                <thead class="border-b border-gray-200 bg-white text-base font-medium text-gray-500">
+                    <tr>
+                        <th class="w-10 px-9 py-3" scope="col">
+                            No.
+                        </th>
+                        <th class="px-6 py-3" scope="col">
+                            <div class="flex flex-row items-center gap-1">
+                                @sortablelink('idSemester', 'Tahun Ajaran')
+
+                                <x-atoms.sorting />
+                            </div>
+                        </th>
+                        <th class="px-6 py-3" scope="col">
+                            <div class="flex flex-row items-center gap-1">
+                                @sortablelink('idKelas', 'Kelas')
+
+                                <x-atoms.sorting />
+                            </div>
+                        </th>
+                        <th class="px-6 py-3" scope="col">
+                            <div class="flex flex-row items-center gap-1">
+                                @sortablelink('NIS', 'Nama Siswa')
+
+                                <x-atoms.sorting />
+                            </div>
+                        </th>
+                        <th class="w-52 px-6 py-3" scope="col">
+                            Aksi
+                        </th>
+                    </tr>
+                </thead>
+
+                @if ($historySiswa->count())
+                    @foreach ($historySiswa as $index => $item)
+                        <tbody>
+                            <tr
+                                class="{{ $index % 2 === 0 ? 'bg-white' : 'bg-blue-100' }} text-base font-medium leading-5">
+                                <th class="px-9" scope="row">
+                                    {{ ($historySiswa->currentPage() - 1) * $historySiswa->perPage() + $loop->iteration }}
+                                </th>
+                                <td class="pl-6">
+                                    {{ $item->semester->tahunajaran->thnAjaran . ' - ' . $item->semester->semester }}
+                                </td>
+                                <td class="pl-6">
+                                    {{ $item->kelas->kelas }}
+                                </td>
+                                <td class="pl-6">
+                                    {{ $item->siswa->nmSiswa }}
+                                </td>
+                                <td class="mr-3 flex flex-row items-center gap-5 2xl:m-0">
+                                    <a href="{{ route('historysiswa.show', $item->idHistory) }}">
+                                        <x-atoms.eye :alt="'detail-historysiswa'" />
+                                    </a>
+
+                                    <a href="{{ route('historysiswa.edit', $item->idHistory) }}">
+                                        <x-atoms.pencil :alt="'edit-historysiswa'" />
+                                    </a>
+
+                                    <div x-data="{ show: false }">
+                                        <button class="focus:outline-none" type="button" @click="show = !show">
+                                            <x-atoms.trash :alt="'delete-historysiswa'" />
+                                        </button>
+
+                                        <x-molecules.modaldelete :title="'Apakah Anda akan menghapus history siswa : ' .
+                                            $item->siswa->nmSiswa .
+                                            ' ?'" :action="route('historysiswa.destroy', $item->idHistory)" />
+                                    </div>
+
+                                </td>
+                            </tr>
+                        </tbody>
+                    @endforeach
+                @endif
+            </table>
+
+            <div class="bg-white p-6">
+                {{ $historySiswa->links('vendor.pagination.tailwind') }}
+            </div>
+
+        </div>
+
+    </section>
+
+</x-layouts.app-layout>
